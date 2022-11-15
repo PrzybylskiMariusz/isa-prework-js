06f6c54935f0d7eea9230bd8183c129f3ba94cd7
Mariusz Przybylski <przybylski.mariusz1987@gmail.com>
2022-11-15 17:33:44 +0100

    Implement slider behavior for arrows

--- examples.js
+++
@@ -1,6 +0,0 @@
-let myFirstFunction = (name, greeting) => {
-	console.log(greeting + "! Mam na imię " + name + "!");
-};
-
-myFirstFunction("Mariusz", "Witaj");
-myFirstFunction("Jan", "Dzień dobry");
