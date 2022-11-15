2dac75ae15c1582ad60988a8118d9faa565f4b29
Mariusz Przybylski <przybylski.mariusz1987@gmail.com>
2022-11-15 17:11:06 +0100

    Create changing slide mechanism for dots

---
+++ examples.js
@@ -0,0 +1,6 @@
+let myFirstFunction = (name, greeting) => {
+	console.log(greeting + "! Mam na imię " + name + "!");
+};
+
+myFirstFunction("Mariusz", "Witaj");
+myFirstFunction("Jan", "Dzień dobry");
